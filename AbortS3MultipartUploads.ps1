9687004e3391c73d189ea9d080f0d4c8ea6ce332
unknown <amitkul@AMITKUL-CARBON.redmond.corp.microsoft.com>
2015-11-11 12:02:46 -0800

    first commit

---
+++ AbortS3MultipartUploads.ps1
@@ -0,0 +1,19 @@
+﻿$outstandingMultiPartUploads = 0;
+
+Do
+{
+    $content = scripts\aws s3api list-multipart-uploads --bucket <bucket-name> | ConvertFrom-Json 
+    $uploads = $content.Uploads
+    $outstandingMultiPartUploads = $uploads.Count
+
+    Write-Host $(Get-Date -Format u): --- Recieved $outstandingMultiPartUploads outstanding uploads to abort ---
+
+
+    $uploads | ForEach-Object{
+        scripts\aws s3api abort-multipart-upload --bucket <bucket-name> --key $_.Key --upload-id $_.UploadId 
+        Write-Host $(Get-Date -Format u): Done: $_.Key
+    }
+
+    Write-Host $(Get-Date -Format u): --- Aborted $outstandingMultiPartUploads outstanding uploads ---
+
+} Until($outstandingMultiPartUploads -eq 0)
---
+++ README.md
@@ -0,0 +1,10 @@
+#Powershell scripts that aborts S3 Multipart Uploads in a bucket
+
+## Prereqs
+* AWS CLI https://aws.amazon.com/cli/
+* PowerShell
+
+## Instructions
+Set up aws cli with your credentials properly
+
+Change the <bucket-name> to your S3 bucket
